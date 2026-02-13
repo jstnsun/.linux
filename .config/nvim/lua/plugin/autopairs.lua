ed5a57a613b9882d8f907b0d418da36721fef0ef
jstnsun <132972461+jstnsun@users.noreply.github.com>
2026-02-13 14:34:52 -0500

    `nvim/.../autopairs.lua` (init)
    This commit adds the secondary `nvim` autopairs plugin configuration
    file to the repository. It is used to configure autopairs typing.
    For more information about `autopairs`, see:
    - https://github.com/windwp/nvim-autopairs
    
    This configuration currently does not differ from the default.

---
+++ .config/nvim/lua/plugin/autopairs.lua
@@ -0,0 +1,12 @@
+-- autopairs.lua
+--
+-- Configuration file for an autopairs plugin via lazy for Neovim.
+-- See https://github.com/windwp/nvim-autopairs for more information.
+--
+-- jstnsun
+
+return {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+}
