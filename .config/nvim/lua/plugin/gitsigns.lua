13371940737634fda54bf48890df5b9ebddb562f
jstnsun <132972461+jstnsun@users.noreply.github.com>
2026-02-17 23:53:06 -0500

    `nvim/.../gitsigns.lua` (change): move on_attach
    This commit moves `on_attach()` from an anonymous function to a local
    function for better readability.

--- .config/nvim/lua/plugin/gitsigns.lua
+++ .config/nvim/lua/plugin/gitsigns.lua
@@ -12,6 +12,43 @@ local signs_tbl = {
     changedelete = { text = "┇" },
 }
 
+-- On-attach function used to configure gitsigns keymaps.
+local function gitsigns_on_attach(bufnr)
+    local gs = package.loaded.gitsigns
+    local function keymap(m, l, r, d) vim.keymap.set(m, l, r, { buffer = bufnr, desc = d }) end
+
+    keymap("n", "[c", function()
+        if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+        else
+            gs.nav_hunk("prev")
+        end
+    end, "jump to previous [c]hange")
+    keymap("n", "]c", function()
+        if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+        else
+            gs.nav_hunk("next")
+        end
+    end, "jump to next [c]hange")
+    keymap("n", "[C", function() gs.nav_hunk("last") end, "jump to last [C]hange")
+    keymap("n", "]C", function() gs.nav_hunk("first") end, "jump to first [C]hange")
+    keymap("n", "<leader>ge", gs.stage_hunk, "(un)stag[e] hunk")
+    keymap("n", "<leader>gr", gs.reset_hunk, "[r]eset hunk")
+    keymap("n", "<leader>gE", gs.stage_buffer, "stag[E] buffer")
+    keymap("n", "<leader>gR", gs.reset_buffer, "[R]eset buffer")
+    keymap("n", "<leader>gp", gs.preview_hunk, "preview hunk [p]oppup")
+    keymap("n", "<leader>gi", gs.preview_hunk_inline, "preview hunk [i]nline")
+    keymap("n", "<leader>gb", gs.blame, "blame [b]uffer")
+    keymap("n", "<leader>gn", gs.blame_line, "blame li[n]e")
+    keymap("n", "<leader>gd", gs.diffthis, "[d]iff against index")
+    keymap("n", "<leader>gD", function() gs.diffthis("~") end, "[D]iff against ~")
+    keymap("n", "<leader>gq", gs.setqflist, "view hunks in [q]uickfix list")
+    keymap("n", "<leader>gw", gs.setloclist, "vie[w] hunks in location list")
+    keymap("n", "<leader>gt", gs.toggle_word_diff, "[t]oggle show word diff")
+    keymap("n", "<leader>gT", gs.toggle_current_line_blame, "[T]oggle show blame line")
+end
+
 return {
     "lewis6991/gitsigns.nvim",
     cmd = "Gitsigns",
@@ -26,46 +63,6 @@ return {
             ignore_whitespace = true,
         },
         current_line_blame_formatter = "<author>, <author_time:%R>",
-        on_attach = function(bufnr)
-            local gs = package.loaded.gitsigns
-            local function keymap(m, l, r, d) vim.keymap.set(m, l, r, { buffer = bufnr, desc = d }) end
-
-            keymap("n", "[c", function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "[c", bang = true })
-                else
-                    gs.nav_hunk("prev")
-                end
-            end, "jump to previous [c]hange")
-            keymap("n", "]c", function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "]c", bang = true })
-                else
-                    gs.nav_hunk("next")
-                end
-            end, "jump to next [c]hange")
-            keymap("n", "[C", function() gs.nav_hunk("last") end, "jump to last [C]hange")
-            keymap("n", "]C", function() gs.nav_hunk("first") end, "jump to first [C]hange")
-
-            keymap("n", "<leader>ge", gs.stage_hunk, "(un)stag[e] hunk")
-            keymap("n", "<leader>gr", gs.reset_hunk, "[r]eset hunk")
-            keymap("n", "<leader>gE", gs.stage_buffer, "stag[E] buffer")
-            keymap("n", "<leader>gR", gs.reset_buffer, "[R]eset buffer")
-
-            keymap("n", "<leader>gp", gs.preview_hunk, "preview hunk [p]oppup")
-            keymap("n", "<leader>gi", gs.preview_hunk_inline, "preview hunk [i]nline")
-
-            keymap("n", "<leader>gb", gs.blame, "blame [b]uffer")
-            keymap("n", "<leader>gn", gs.blame_line, "blame li[n]e")
-
-            keymap("n", "<leader>gd", gs.diffthis, "[d]iff against index")
-            keymap("n", "<leader>gD", function() gs.diffthis("~") end, "[D]iff against ~")
-
-            keymap("n", "<leader>gq", gs.setqflist, "view hunks in [q]uickfix list")
-            keymap("n", "<leader>gw", gs.setloclist, "vie[w] hunks in location list")
-
-            keymap("n", "<leader>gt", gs.toggle_word_diff, "[t]oggle show word diff")
-            keymap("n", "<leader>gT", gs.toggle_current_line_blame, "[T]oggle show blame line")
-        end,
+        on_attach = gitsigns_on_attach,
     },
 }
