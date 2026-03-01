311f0bd3f4abc3b2ee5e37cf4cb6b09fa1221ab3
jstnsun <132972461+jstnsun@users.noreply.github.com>
2026-02-28 23:39:06 -0500

    `nvim/.../after.lua` (init)
    This commit adds the secondary `nvim` after configuration file to the
    repository. It is used to configure core settings after configurations
    in `core` have been set, and meant to mimic the `after` directory.
    For more information about `nvim` `after`, see:
    - https://neovim.io/doc/user/options/#after-directory
    
    This configuration currently differs from the default by containing
    explicit differences for all contents.

---
+++ .config/nvim/lua/core/after.lua
@@ -0,0 +1,9 @@
+-- after.lua
+--
+-- Configuration file that loads configurations after core ones for Neovim.
+-- It is meant to mimic the `after` directory, but as a file for core configs.
+-- See https://neovim.io/doc/user/options/#after-directory for more information.
+--
+-- jstnsun
+
+vim.api.nvim_command("colorscheme tokyonight")
